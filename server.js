148fa0c16b0034447d086b0f0a85ff3dc73caed4
mosstfa1239-wq <__>
2026-07-18 23:14:56 +0300

    check hash

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
-const axios = require("axios");
-const crypto = require("crypto");
 const { Resend } = require("resend");
 
 const crypto = require("crypto");
